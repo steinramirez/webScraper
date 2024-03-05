527708a1afa1df6f098b1563dd56a7e3065c1a44
Stein <steinvlza@gmail.com>
2024-03-04 21:58:44 -0500

    add files

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, render_template, send_file, make_response
 from bs4 import BeautifulSoup
 import requests
+import os
 
 app = Flask(__name__)
 
+def process_url(url):
+    if not url.startswith('http'):
+        url = 'https://' + url
+    return url
+
 @app.route('/',  methods=['GET', 'POST'])
 def index():
+    results = []  # Initialize results list
     if request.method == 'POST':
-        url = request.form['url']
-        data = request.form['data']
-        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
-        results = soup.find_all(data)
-        return render_template('index.html', results=results)
-    else:
-        return render_template('index.html')
+        if 'clear' in request.form:
+            return render_template('index.html')  # Render the template without results
+        elif 'download' in request.form:
+            # Generate report in a text file
+            report_file = open('report.txt', 'w')
+            report_file.write('Report for URL: {}\n'.format(request.form['url']))
+            report_file.write('Data: {}\n'.format(request.form['data']))
+            report_file.write('- {}\n'.format(request.form[results]))
+            for result in results:
+                report_file.write('- {}\n'.format(result))
+            report_file.close()
+            # Send the report file for download
+            return send_file('report.txt', as_attachment=True)
+        else:
+            url = request.form['url']
+            data = request.form['data']
+            soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+            results = soup.find_all(data)
+    return render_template('index.html', results=results)
 
 @app.route('/download', methods=['GET'])
 def download(results):
