6a942aa59b83621383eb18041a3e68f2f73e3622
Stein <steinvlza@gmail.com>
2024-03-14 19:56:10 -0500

    styles and interface updates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, send_file, make_response
 from bs4 import BeautifulSoup
 import requests
 import os
+import json
 
 app = Flask(__name__)
 
@@ -23,12 +24,12 @@ def index():
             report_file.write('Data: {}\n'.format(request.form['data']))
             report_file.write('- {}\n'.format(request.form[results]))
             for result in results:
-                report_file.write('- {}\n'.format(result))
+                report_file.write('- {}\n'.format(result.name))
             report_file.close()
             # Send the report file for download
             return send_file('report.txt', as_attachment=True)
         else:
-            url = request.form['url']
+            url = process_url(request.form['url'])
             data = request.form['data']
             soup = BeautifulSoup(requests.get(url).content, 'html.parser')
             results = soup.find_all(data)
@@ -46,6 +47,12 @@ def download(results):
     response.headers['Content-Type'] = 'text/plain'
     response.headers['Content-Disposition'] = 'attachment; filename=report.txt'
     return response
+
+@app.route('/faq')
+def faq():
+    with open('static/faq.json', 'r') as file:
+        faq_data = json.load(file)
+    return render_template('faq.html', faq_data=faq_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
