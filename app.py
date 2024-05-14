6cc69ffac559c37396296840239acef9347d3e87
Stein <steinvlza@gmail.com>
2024-05-14 13:37:01 -0500

    updates for the last version of webb scraper

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def faq():
     return render_template('faq.html', faq_data=faq_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    content: [
+      './templates/**/*.{html,tmpl}',
+      './static/css/*.css'
+    ],
+    theme: {
+      extend: {}
+    },
+    variants: {},
+    plugins: []
+  };
