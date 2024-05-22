f65d15f8f4c09dc009fab4f6e6d67cee7c34108a
Stein <steinvlza@gmail.com>
2024-05-21 23:07:46 -0500

    copy function deployed for each li on results, styles for desktop and mobile fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_file, make_response
+from flask import url_for, Flask, send_from_directory,  request, render_template, send_file, make_response, session
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -33,20 +33,21 @@ def index():
             data = request.form['data']
             soup = BeautifulSoup(requests.get(url).content, 'html.parser')
             results = soup.find_all(data)
-    return render_template('index.html', results=results)
+    return render_template('index.html', results=results, favicon_url=url_for('static', filename='favicon.ico'))
 
 @app.route('/download', methods=['GET'])
-def download(results):
-    # Create a string that contains the data you want to download
-    data = "This is the data that will be downloaded in a text file.\n"
-    for result in results:
-        data += str(result) + "\n"
-
-    # Set the Content-Type and Content-Disposition headers
-    response = make_response(data)
-    response.headers['Content-Type'] = 'text/plain'
-    response.headers['Content-Disposition'] = 'attachment; filename=report.txt'
-    return response
+def download():
+    if 'results' in session:
+        results = session['results']
+        data = "This is the data that will be downloaded in a text file.\n"
+        for result in results:
+            data += str(result) + "\n"
+        response = make_response(data)
+        response.headers['Content-Type'] = 'text/plain'
+        response.headers['Content-Disposition'] = 'attachment; filename=report.txt'
+        return response
+    else:
+        return 'No results to download', 404
 
 @app.route('/faq')
 def faq():
@@ -54,7 +55,10 @@ def faq():
         faq_data = json.load(file)
     return render_template('faq.html', faq_data=faq_data)
 
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+        'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
